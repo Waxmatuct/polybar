698a924e044240ee21cd3e411541631236d8fcdd
Waxmatuct <alvheim@rambler.ru>
2021-03-15 00:35:20 +0300

    первый коммит

---
+++ scripts/currencyrates.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import json,sys,urllib.request,time
+
+if len(sys.argv) != 3:
+    print("Usage: ./currencyrates.py comma_separated_currency_list base_currency. Example: ./currencyrates.py usd,eur,btc nok")
+    sys.exit()
+
+currencies = sys.argv[1]
+basecurrency = sys.argv[2]
+
+currencyurl = "http://freecurrencyrates.com/api/action.php?do=cvals&iso=" + currencies.replace(',','') + "&f=" + basecurrency + "&v=1&s=cbr"
+f = urllib.request.urlopen(currencyurl)
+obj = json.loads(f.read())
+res="";
+for c in currencies.split(','):
+    res += c.upper() + " {:,.2f}".format(1/obj[c.upper()]).replace(',',' ')
+
+# some unicode currency code replacement (optional)
+res = res.replace("USD", "")
+res = res.replace("EUR", "")
+res = res.replace("BTC", "")
+
+sys.stdout.write(res);
